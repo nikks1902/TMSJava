680f77be89a44520e948c6cbaa93501d3bd43081
nikks1902 <nikoreal1902@gmail.com>
2023-02-06 16:00:00 +0300

    Small changes

--- src/by/teachmeskills/homeworks/hw_100223/FirstHWClass.java
+++ src/by/teachmeskills/homeworks/hw_100223/FirstHWClass.java
@@ -2,8 +2,8 @@ package by.teachmeskills.homeworks.hw_100223;
 
 public class FirstHWClass {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 8;
+        int a = 11;
+        int b = 9;
         int sum = a - b;
         System.out.println("Sum: " + sum);
     }
