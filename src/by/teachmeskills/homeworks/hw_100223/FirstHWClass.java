b34b040fa0e564998410f4fb426dac1345b04795
NIKOLAY AWDEYENKO <nikks1902@protonmail.com>
2023-02-06 13:13:55 +0300

    Initial commit

---
+++ src/by/teachmeskills/homeworks/hw_100223/FirstHWClass.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_100223;
+
+public class FirstHWClass {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 8;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
