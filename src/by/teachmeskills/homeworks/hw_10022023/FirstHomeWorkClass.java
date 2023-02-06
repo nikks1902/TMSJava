edc2dc49129012e2fe1ae5308f10169023dfb8d2
nikks1902 <nikoreal1902@gmail.com>
2023-02-06 16:55:13 +0300

    Initial commit

---
+++ src/by/teachmeskills/homeworks/hw_10022023/FirstHomeWorkClass.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_10022023;
+
+public class FirstHomeWorkClass {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 17;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
