acc48bc87389f37131e24fe3ae30950a68af35fd
nikks1902 <nikoreal1902@gmail.com>
2023-02-16 21:10:53 +0300

    HW17022023 task 2 switch

--- src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task2_switch.java
+++ src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task2_switch.java
@@ -24,7 +24,6 @@ public class HomeWork17022023_task2_switch {
                 break;
             case 7:
                 System.out.println("Sunday");
-                break;
             default:
                 throw new IllegalArgumentException("Not found!");
         }
