e0f3b725ca9b26b543f07e62944d2923b1085587
nikks1902 <nikoreal1902@gmail.com>
2023-02-16 20:50:14 +0300

    HW17022023 task 1

--- src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task1.java
+++ src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task1.java
@@ -7,7 +7,7 @@ public class HomeWork17022023_task1 {
         System.out.println("side1, side2, radius");
         Scanner console = new Scanner(System.in);
         int num = console.nextInt();
-        int side1 = 10, side2 = 5, radius = 7;
+        int side1 = 10, side2 = 5, radius = 8;
         if ((side1*side1)+(side2*side2)<=(radius*2)*(radius*2))
         System.out.println("The hole can be completely covered");
         else System.out.println("The hole cannot be completely covered");
