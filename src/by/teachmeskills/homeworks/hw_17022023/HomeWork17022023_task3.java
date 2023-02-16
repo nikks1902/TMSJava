51b87104267712b9e0ebbedc999a41785c96d8b9
nikks1902 <nikoreal1902@gmail.com>
2023-02-16 22:50:50 +0300

    HW17022023 task 3 cycles

---
+++ src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task3.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.homeworks.hw_17022023;
+
+public class HomeWork17022023_task3 {
+    public static void main(String[] args) {
+        int i = 4;
+
+        while (i > 0) {
+            System.out.println("****");
+            i--;
+        }
+    }
+}
