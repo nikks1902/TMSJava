adbb4e1846edf2122384f2bce6045e2cbe430f48
nikks1902 <nikoreal1902@gmail.com>
2023-02-25 11:38:01 +0300

    HW17022023 task 4

--- src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task4.java
+++ src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task4.java
@@ -2,29 +2,51 @@ package by.teachmeskills.homeworks.hw_17022023;
 
 public class HomeWork17022023_task4 {
     public static void main(String[] args) {
+        int[] array = {7, -5, 8, 15, -10, 20};
         int min = 0;
         int max = 0;
         int sum = 0;
-        int x = 1;
-        int[] array = {7, -5, 8, 15, -10, 20};
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-                System.out.println(min);
-            }
-            if (array[i] > max) {
-                max = array[i];
-                System.out.println(max);
-            }
-            if (array[i] < 0) {
-                sum += array[i];
-                System.out.println(sum);
-            }
-            if (array[i] > (min)) {
-                if (array[i] < (max)) {
-                    x*=array[i];
+        int minIndex = 0;
+        int maxIndex = 0;
+        int product = 1;
+
+        boolean sorted = false;
+        int temp;
+
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    sorted = false;
                 }
             }
         }
+
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+            if (array[j] > array[maxIndex]) {
+                maxIndex = j;
+            }
+            product *= array[j];
+        }
+        System.out.println(product);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < 0) {
+                sum += array[i];
+            }
+        }
+        System.out.println(sum);
     }
 }
