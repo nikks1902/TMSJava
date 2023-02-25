9e3869b2fd5d5e620da7d6701a36b9e6ece3362e
nikks1902 <nikoreal1902@gmail.com>
2023-02-25 18:24:08 +0300

    HW17022023 task 7

--- src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task7.java
+++ src/by/teachmeskills/homeworks/hw_17022023/HomeWork17022023_task7.java
@@ -2,10 +2,9 @@ package by.teachmeskills.homeworks.hw_17022023;
 
 public class HomeWork17022023_task7 {
     public static void main(String[] args) {
-        double[][] num = new double[10][];
-
+        double[][] num = new double[9][];
         for (int i = 0; i < num.length; i++){
-            num[i] = new double[i + 1];
+            num[i] = new double[]{Math.random()};
             for (int j = 0; j < num[i].length; j++){
                 num[i][j] = Math.random();
                 System.out.print(num[i][j]);
