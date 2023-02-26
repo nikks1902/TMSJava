9ab509222757d92e614e26e5d1ddb27698c87a08
nikks1902 <nikoreal1902@gmail.com>
2023-02-26 11:08:18 +0300

    HW17022023 task 3

---
+++ src/by/teachmeskills/homeworks/hw_170223/HW170223_task3.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.homeworks.hw_170223;
+
+public class HW170223_task3 {
+    public static void main(String[] args) {
+        int i = 4;
+
+        while (i > 0) {
+            System.out.println("****");
+            i--;
+        }
+    }
+}
