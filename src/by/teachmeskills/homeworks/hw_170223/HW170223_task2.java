2ff630880d6915eb1d240e00580db8b9b65cc85e
nikks1902 <nikoreal1902@gmail.com>
2023-02-26 11:04:19 +0300

    HW17022023 task 2

---
+++ src/by/teachmeskills/homeworks/hw_170223/HW170223_task2.java
@@ -0,0 +1,32 @@
+package by.teachmeskills.homeworks.hw_170223;
+
+public class HW170223_task2 {
+    public static void main(String[] args) {
+        int dayOfTheWeek = 7;
+        switch (dayOfTheWeek){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+
+
+    }
+}
