d76125cb0a98ed14db4b1ccf20782bf058c12769
nikks1902 <nikoreal1902@gmail.com>
2023-03-08 15:27:34 +0300

    HW17022023 Task 6 Two Dimensional Array

--- src/by/teachmeskills/homeworks/hw_170223/Task6_TwoDimensionalArray.java
+++ src/by/teachmeskills/homeworks/hw_170223/Task6_TwoDimensionalArray.java
@@ -17,10 +17,11 @@ public class Task6_TwoDimensionalArray {
                 array[i][j] = random.nextInt(20) + 1;
             }
         }
-        for (int i = 0; i < array.length; i++, System.out.println()) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
+            System.out.println();
         }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
